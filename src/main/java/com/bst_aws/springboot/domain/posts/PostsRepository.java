335a872f184b6f8e2b35667ae487466ac3d39226
bgwb0007 <bgwb0007@gmail.com>
2021-01-27 11:53:42 +0900

    Create JPA TestCode

---
+++ src/main/java/com/bst_aws/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.bst_aws.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
