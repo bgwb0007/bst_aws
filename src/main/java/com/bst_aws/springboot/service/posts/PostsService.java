a06668b8cb052d8f5446a082714fcf91a62b2272
bgwb0007 <bgwb0007@gmail.com>
2021-03-06 17:17:20 +0900

    add BaseTimeEntity

--- src/main/java/com/bst_aws/springboot/service/posts/PostsService.java
+++ src/main/java/com/bst_aws/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.bst_aws.springboot.service.posts;
 
 import com.bst_aws.springboot.domain.posts.Posts;
 import com.bst_aws.springboot.domain.posts.PostsRepository;
+import com.bst_aws.springboot.web.dto.PostsListResponseDto;
 import com.bst_aws.springboot.web.dto.PostsResponseDto;
 import com.bst_aws.springboot.web.dto.PostsSaveRequestDto;
 import com.bst_aws.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,9 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +36,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
