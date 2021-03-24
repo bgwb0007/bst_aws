c789c1a94a0c00c7b1c0b7db1c732d224c62b006
bgwb0007 <bgwb0007@gmail.com>
2021-03-24 14:52:19 +0900

    test success

--- src/test/java/com/bst_aws/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/bst_aws/springboot/web/PostsApiControllerTest.java
@@ -39,9 +39,6 @@ public class PostsApiControllerTest {
     private int port;
 
     @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Autowired
     private PostsRepository postsRepository;
 
     @Autowired
