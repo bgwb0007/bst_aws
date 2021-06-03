4ab968f495a165399855ace8a84852b668dc590f
bgwb0007 <bgwb0007@gmail.com>
2021-06-03 17:23:31 +0900

    login page 추가

--- src/main/resources/static/js/app/post_save.js
+++ src/main/resources/static/js/app/post_save.js
@@ -5,13 +5,6 @@ var main = {
             _this.save();
         });
 
-        $('#btn-update').on('click', function () {
-            _this.update();
-        });
-
-        $('#btn-delete').on('click', function () {
-            _this.delete();
-        });
     },
     save : function () {
         var data = {
@@ -31,45 +24,6 @@ var main = {
         }).done(function() {
             alert('글이 등록되었습니다.');
             window.location.href = '/post/';
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        });
-    },
-    update : function () {
-        var data = {
-            content: $('#content').val(),
-            createdBy : $('#createdBy').val(),
-            dDay : $('#dDay').val(),
-            district : $('#district').val(),
-            title : $('#title').val()
-        };
-
-        var id = $('#id').val();
-
-        $.ajax({
-            type: 'PUT',
-            url: '/api/v2/post/'+id,
-            dataType: 'json',
-            contentType:'application/json; charset=utf-8',
-            data: JSON.stringify(data)
-        }).done(function() {
-            alert('글이 수정되었습니다.');
-            window.location.href = '/';
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        });
-    },
-    delete : function () {
-        var id = $('#id').val();
-
-        $.ajax({
-            type: 'DELETE',
-            url: '/api/v2/post/'+id,
-            dataType: 'json',
-            contentType:'application/json; charset=utf-8'
-        }).done(function() {
-            alert('글이 삭제되었습니다.');
-            window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
