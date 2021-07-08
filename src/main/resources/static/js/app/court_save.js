35a705ad16e318a391938a50195b100863762f0e
bgwb0007 <bgwb0007@gmail.com>
2021-07-09 00:04:48 +0900

    court_save.js

---
+++ src/main/resources/static/js/app/court_save.js
@@ -0,0 +1,35 @@
+var main = {
+    init : function () {
+        var _this = this;
+        $('#btn-court-save').on('click', function () {
+            _this.save();
+        });
+
+    },
+    save : function () {
+        var data = {
+            name: $('#inputCourtName').val(),
+            e : $('#inputCourtE').val(),
+            n : $('#inputCourtN').val(),
+            address : $('#inputCourtAddress').val(),
+            web : $('#inputCourtWeb').val(),
+            info : $('#inputCourtInfo').val(),
+            district : $('#inputCourtInfo').val()
+        };
+        $.ajax({
+            type: 'POST',
+            url: '/api/v2/court',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 등록되었습니다.');
+            window.location.href = '/admin/court';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    }
+
+};
+
+main.init();
