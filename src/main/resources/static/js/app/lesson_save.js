3b5233c55ae552ad6f2e1375983a9cad88990394
bgwb0007 <bgwb0007@gmail.com>
2021-07-15 23:35:07 +0900

    lesson_save.js 추가

--- src/main/resources/static/js/app/lesson_save.js
+++ src/main/resources/static/js/app/lesson_save.js
@@ -1,30 +1,30 @@
 var main = {
     init : function () {
         var _this = this;
-        $('#btn-court-save').on('click', function () {
+        $('#btn-lesson-save').on('click', function () {
             _this.save();
         });
 
     },
     save : function () {
         var data = {
-            name: $('#inputCourtName').val(),
-            e : $('#inputCourtE').val(),
-            n : $('#inputCourtN').val(),
-            address : $('#inputCourtAddress').val(),
-            web : $('#inputCourtWeb').val(),
-            info : $('#inputCourtInfo').val(),
-            district : $('#inputCourtDistrict').val()
+            name: $('#inputLessonName').val(),
+            e : $('#inputLessonE').val(),
+            n : $('#inputLessonN').val(),
+            address : $('#inputLessonAddress').val(),
+            web : $('#inputLessonWeb').val(),
+            info : $('#inputLessonInfo').val(),
+            district : $('#inputLessonDistrict').val()
         };
         $.ajax({
             type: 'POST',
-            url: '/api/v2/court',
+            url: '/api/v2/lesson',
             dataType: 'json',
             contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function() {
             alert('코트가 등록되었습니다.');
-            window.location.href = '/admin/court';
+            window.location.href = '/admin/lesson';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
