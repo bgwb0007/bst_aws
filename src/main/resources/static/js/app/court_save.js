33f3fdcc9aa1592c4f421e74aa987fc30dc83f81
bgwb0007 <bgwb0007@gmail.com>
2021-07-23 20:14:26 +0900

    court_save.js delete 추가

--- src/main/resources/static/js/app/court_save.js
+++ src/main/resources/static/js/app/court_save.js
@@ -4,6 +4,9 @@ var main = {
         $('#btn-court-save').on('click', function () {
             _this.save();
         });
+        $('#btn-court-delete').on('click',function (){
+            _this.delete();
+        })
 
     },
     save : function () {
@@ -24,6 +27,21 @@ var main = {
             data: JSON.stringify(data)
         }).done(function() {
             alert('코트가 등록되었습니다.');
+            window.location.href = '/admin/court';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    delete : function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v2/court/'+id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8'
+        }).done(function() {
+            alert('해당 코트가 삭제되었습니다.');
             window.location.href = '/admin/court';
         }).fail(function (error) {
             alert(JSON.stringify(error));
